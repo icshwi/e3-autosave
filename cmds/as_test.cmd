073536bf4e40e56d66a9551e86b89eee3cd3d224
Jeong Han Lee <jeonghan.lee@gmail.com>
2019-03-18 18:34:28 +0100

    update autosave, and add test db

---
+++ cmds/as_test.cmd
@@ -0,0 +1,25 @@
+require iocStats,ae5d083
+require autosave,5.9.0
+require std,3.5.0
+
+epicsEnvSet("TOP", "$(E3_CMD_TOP)/..")
+
+epicsEnvSet("SEC", "SEC")
+epicsEnvSet("SUB", "SUB01")
+epicsEnvSet("P", "$(SEC)-$(SUB):")
+epicsEnvSet("DIS", "DIS")
+epicsEnvSet("DEV", "DEV-01")
+epicsEnvSet("R", "$(DIS)-$(DEV)")
+epicsEnvSet("IOCNAME", "ioc_test")
+
+
+#iocshLoad("$(iocStats_DIR)/iocStats.iocsh", "IOCNAME=$(IOCNAME)")
+iocshLoad("$(autosave_DIR)/autosave.iocsh", "AS_TOP=$(TOP),IOCNAME=$(IOCNAME)")
+
+dbLoadRecords("$(TOP)/template/SR_test_info.db","P=$(IOCNAME):,N=12")
+
+
+iocInit()
+
+dbl > "${IOCNAME}_PVs.list"
+
